3be7fb20f9e1dd86badb22444cb4bd2681fc8969
Ricardo.lagos <ricardolagoshn>
2022-06-15 20:48:29 -0600

    Cambios del dia 15/06/2022

--- app/src/main/java/com/aplicacion/pm012pp1/ActivityPhoto.java
+++ app/src/main/java/com/aplicacion/pm012pp1/ActivityPhoto.java
@@ -5,16 +5,25 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class ActivityPhoto extends AppCompatActivity
 {
@@ -23,6 +32,7 @@ public class ActivityPhoto extends AppCompatActivity
     ImageView ObjectImage;
     Button btnfoto;
     String PathFoto;
+    String currentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -82,5 +92,55 @@ public class ActivityPhoto extends AppCompatActivity
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+
+
+        if(requestCode == REQUESTTAKEPHOTO && resultCode == RESULT_OK)
+        {
+            Bundle extraerfoto = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extraerfoto.get("data");
+            ObjectImage.setImageBitmap(imageBitmap);
+        }
     }
+
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        //File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.aplicacion.pm012pp1.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUESTTAKEPHOTO);
+            }
+        }
+    }
+
 }
